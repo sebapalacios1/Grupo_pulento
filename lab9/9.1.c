81bcf727ee29707800544c29c671db0c6df51a77
Javiera Vega P <javiera.vega.p@mail.pucv.cl>
2026-05-11 11:55:25 -0400

    Add files via upload

---
+++ lab9/9.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declaramos las variables con valores cualquiera
+    int edad = 21;
+    float nota = 6.5;
+    char seccion = 'A';
+
+    // Imprimimos usando %d para entero, %f para flotante y %c para caracter
+    printf("Edad: %d\n", edad);
+    printf("Nota del certamen: %f\n", nota);
+    printf("Seccion: %c\n", seccion);
+
+    return 0;
+}
